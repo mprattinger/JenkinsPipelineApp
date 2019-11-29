72119b63924563b4b2c54d862425687f3bfa0084
Michael Prattinger <mprattinger@outlook.com>
2019-11-29 15:46:33 +0100

    Add project files.

---
+++ JenkinsPipelineApp.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+using FluentAssertions;
+using System;
+using Xunit;
+
+namespace JenkinsPipelineApp.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            true.Should().BeTrue();
+        }
+    }
+}
