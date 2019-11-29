8f651e48b29b99ab174ac121a67683079f191e89
Michael Prattinger <mprattinger@outlook.com>
2019-11-29 22:40:29 +0100

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+printf "Copying files to remote server..."
+rsync -avz -e 'ssh' dist/ deploy@nuc:/home/deploy/apps/backgarden
+
+if [ $? -ne 0 ]
+then
+  printf "Error copying files!
+  return 1
+fi
+
+printf "Files copyied! Starting service"
+ssh deploy@nuc "sudo supervisorctl restart backgarden"
